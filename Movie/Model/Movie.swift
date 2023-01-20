ef6a71fa1bccb65d7130e3cf6e92e5826da33e51
Raline <ralinesilva1@gmail.com>
2023-01-20 20:42:22 -0300

    remover dados estaticos

--- Movie/Model/Movie.swift
+++ Movie/Model/Movie.swift
@@ -7,19 +7,24 @@
 
 import UIKit
 
-struct Movie {
-    let id: Int
-    var title: String
-    var overview: String
-    var releaseDate: String
-    var image: String?
-    var voteAverage: Double
+struct MovieResults: Codable {
+    let results: [Movie]
 }
 
-let movies: [Movie] = [
-    Movie(id: 1, title: "Órfã 2: A Origem", overview: "", releaseDate: "2022-07-27", image: nil, voteAverage: 7.2),
-    Movie(id: 2, title: "Minions 2: A Origem de Gru", overview: "", releaseDate: "2022-06-29", image: nil, voteAverage: 7.8),
-    Movie(id: 3, title: "Thor: Amor e Trovão", overview: "", releaseDate: "2022-07-27", image: nil, voteAverage: 6.8),
-    Movie(id: 4, title: "Avatar", overview: "", releaseDate: "2009-12-18", image: nil, voteAverage: 8.8)
+struct Movie: Codable {
+    let id: Int
+    let title: String
+    let overview: String
+    let releaseDate: String
+    let image: String?
+    let voteAverage: Double
     
-]
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case overview
+        case releaseDate = "release_date"
+        case image = "poster_path"
+        case voteAverage = "vote_average"
+    }
+}
