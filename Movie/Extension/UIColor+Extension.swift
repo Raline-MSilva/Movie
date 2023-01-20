0e89835f72a4658ec9a0501cbf612cc6a8024c16
Raline <ralinesilva1@gmail.com>
2023-01-20 20:23:40 -0300

    criar extensao para cor e deixar o fundo gradiente

---
+++ Movie/Extension/UIColor+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extension.swift
+//  Movie
+//
+//  Created by Raline Maria da Silva on 20/01/23.
+//
+
+import UIKit
+
+extension UIColor {
+    static let lightBackground = UIColor(red: 35/255, green: 107/255, blue: 142/255, alpha: 1.0)
+    static let darkBackground = UIColor(red: 19/255, green: 11/255, blue: 43/255, alpha: 1.0)
+}
---
+++ Movie/Extension/UIView+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Extension.swift
+//  Movie
+//
+//  Created by Raline Maria da Silva on 20/01/23.
+//
+
+import UIKit
+
+extension UIView {
+    func setBackground() {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = [UIColor.lightBackground.cgColor, UIColor.darkBackground.cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        self.layer.addSublayer(gradient)
+    }
+}
