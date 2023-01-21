a471035c2562926cf46395d06214983ad97454f8
Raline <ralinesilva1@gmail.com>
2023-01-20 21:04:56 -0300

    criar tela de detalhes do filme

--- Movie/Scenes/MovieDetailsViewController.swift
+++ Movie/Scenes/MovieDetailsViewController.swift
@@ -5,4 +5,96 @@
 //  Created by Raline Maria da Silva on 27/11/22.
 //
 
-import Foundation
+import UIKit
+import Kingfisher
+
+class MovieDetailsViewController: UIViewController, SetupViewCode {
+    
+    private var movie: Movie
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = movie.title
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
+        return label
+    }()
+    
+    private lazy var userRating: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Classificação dos usuários: \(movie.voteAverage)"
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 16.0, weight: .bold)
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textColor = .white.withAlphaComponent(0.75)
+        label.attributedText = NSAttributedString(string: movie.overview).withLineSpacing(7.0)
+        return label
+    }()
+    
+    private lazy var imagePoster: UIImageView = {
+        let imgView = UIImageView()
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        imgView.layer.cornerRadius = 32.0
+        imgView.layer.masksToBounds = true
+        return imgView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.setBackground()
+        setup()
+    }
+    
+    init(movie: Movie) {
+        self.movie = movie
+        super.init(nibName: nil, bundle: nil)
+        guard let imageURL = movie.image else { return }
+        imagePoster.configurePoster(imageURL)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupConfigure() {
+        navigationController?.navigationBar.tintColor = UIColor.white
+    }
+    
+    func setupSubviews() {
+        view.addSubview(titleLabel)
+        view.addSubview(imagePoster)
+        view.addSubview(userRating)
+        view.addSubview(descriptionLabel)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32.0),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            
+            userRating.topAnchor.constraint(equalTo: imagePoster.bottomAnchor, constant: 32.0),
+            userRating.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            imagePoster.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imagePoster.widthAnchor.constraint(equalToConstant: 192.0),
+            imagePoster.heightAnchor.constraint(equalToConstant: 264.0),
+            imagePoster.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 32.0),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: userRating.bottomAnchor, constant: 32.0),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16.0),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16.0),
+        ])
+    }
+}
